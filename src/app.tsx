cf4b970f3c526bac5be9de408dfc4cb8d385168a
김태강 <k_2684@naver.com>
2023-08-19 04:45:42 +0900

    release: 1.0.3 version

--- src/app.tsx
+++ src/app.tsx
@@ -3,31 +3,33 @@ import { Box, Text } from 'ink';
 import Gradient from 'ink-gradient';
 import BigText from 'ink-big-text';
 
-export default function App() {
+export default function App({ name }: { name: string }) {
   return (
     <>
       <Gradient name="instagram">
         <BigText text="Grayash" align="center" />
       </Gradient>
       <Box borderStyle="single" padding={2} flexDirection="column" alignItems="center">
-        <Text color={'magenta'}>Hello, </Text>
-        <Text color={'magentaBright'}>I'm </Text>
-        <Text bold={true} color={'white'} backgroundColor={'magenta'}>
-          Grayash
-        </Text>
-        <Box marginBottom={1} flexDirection={'column'}>
-          <Text color={'grey'}>Front-End Engineer</Text>
-          <Text color={'grey'}>React, React Native, Next.js, TypeScript</Text>
+        <Box marginBottom={1} alignItems="center">
+          <Text color={'magenta'}>Hello, {name} </Text>
+          <Text color={'magentaBright'}>I'm </Text>
+          <Text bold={true} color={'white'} backgroundColor={'magenta'}>
+            Grayash
+          </Text>
+        </Box>
+        <Box marginBottom={1} flexDirection="column" alignItems="center">
+          <Text color="grey">Front-End Engineer</Text>
+          <Text color="grey">React, React Native, Next.js, TypeScript</Text>
         </Box>
         <Box flexDirection="column" marginBottom={1} alignItems="center">
           <Text>
-            <Text backgroundColor={'whiteBright'} color={'black'} underline={false}>
+            <Text backgroundColor="whiteBright" color="black" underline={false}>
               GitHub
             </Text>
             <Text underline={true}> https://github.com/grayashh</Text>
           </Text>
           <Text>
-            <Text backgroundColor={'whiteBright'} color={'black'} underline={false}>
+            <Text backgroundColor="whiteBright" color="black" underline={false}>
               Homepage
             </Text>
             <Text underline={true}> https://grayash.vercel.app</Text>
